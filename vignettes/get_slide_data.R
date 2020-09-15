d9dcd701947149f8b8e798bfedbdba068956b757
Léo Belzile <belzilel@gmail.com>
2020-09-14 21:56:08 -0400

    Add material for week 3 modulo code

--- vignettes/get_slide_data.R
+++ vignettes/get_slide_data.R
@@ -1,22 +1,41 @@
-slides <- list.files(path = "/home/lbelzile/Documents/Dropbox/website/MATH60604A-slides", pattern = "*.html")
-slides <- slides[substr(slides, 1, 12) == "MATH60604A_w"]
-ns <- length(slides)
-codedir <- "../code"
+index <- c("0_","1a","1b","1c","2a","2b","2c","2d","3_")
+index0 <- c("0","1a","1b","1c","2a","2b","2c","2d","3")
 names <- c("Course Outline",
            "Hypothesis Tests",
            "Central Limit Theorem",
            "Exploratory Data Analysis",
            "Parameter interpretation (linear model)",
            "Linear transformations",
-           "Geometry of least squares")
-week <- as.numeric(substr(slides, 13,13))
+           "Geometry of least squares",
+           "Hypothesis tests (linear model)",
+           "Maximum likelihood estimation")
+ns <- length(index)
+url <- "https://lbelzile.github.io/MATH60604A-slides/"
+codedir <- "../code"
+
+slides <- list.files(path = "/home/lbelzile/Documents/Dropbox/website/MATH60604A-slides", pattern = "*.html")
+slides <- slides[substr(slides, 1, 12) == "MATH60604A_w"]
+sl <- rep("", ns)
+pmasl <- na.omit(pmatch(index, substr(slides, start = 13, stop = 14)))
+sl[pmasl] <- paste0("[html](",url, slides,")")
+
+
+slidespdf <- list.files(path = "/home/lbelzile/Documents/Dropbox/website/MATH60604A-slides", pattern = ".*.pdf")
+slidespdf <- slidespdf[substr(slidespdf, 1, 12) == "MATH60604A_w"]
+slpdf <- rep("", ns)
+pmaslp <- na.omit(pmatch(index, substr(slidespdf, start = 13, stop = 14)))
+slpdf[pmaslp] <- paste0("[pdf](",url, slidespdf,")")
+
+
 video <- c("https://youtu.be/luOkCcpDSjs",
             "https://youtu.be/TSMuEX8FqYo",
             "https://youtu.be/nCUT05szKwQ",
             "https://youtu.be/5Yc46pAQpFk",
             "https://youtu.be/4jOnZrnPlUM",
             "https://youtu.be/MzmS9r-77lI",
-           "https://youtu.be/F9dR6mpOVtI"
+            "https://youtu.be/F9dR6mpOVtI",
+            "https://youtu.be/PatlZ9mlVuk",
+            "https://youtu.be/IO3et3Uk4mQ"
            )
 if(length(names) - length(video) > 0){
   video <- c(video, rep("", length(names) - length(video)))
@@ -25,18 +44,19 @@ linkgithub <- "https://raw.githubusercontent.com/lbelzile/statmod/master/"
 
 codesas <- list.files(path = codedir, pattern = "MATH60604A.*.sas")
 codestr <- rep("", ns)
-nid <- sapply(substr(codesas,12,13), function(x){which(x == substr(slides[-1], 13,14))})
-codestr[nid+1L] <- paste0("[SAS](",linkgithub, "code/", codesas,")")
+nid <- sapply(substr(codesas,12,13), function(x){which(x == index)})
+codestr[nid] <- paste0("[SAS](",linkgithub, "code/", codesas,")")
 
 codeR <- list.files(path = codedir, pattern = "*.R")
 codeRstr <- rep("", ns)
-nid <- sapply(substr(codeR,12,13), function(x){which(x == substr(slides[-1], 13,14))})
-codeRstr[nid+1L] <- paste0("[R](",linkgithub,"code/", codeR,")")
+nid <- sapply(substr(codeR,12,13), function(x){pmatch(x, index)})
+codeRstr[nid] <- paste0("[R](",linkgithub,"code/", codeR,")")
 
 
-url <- "https://lbelzile.github.io/MATH60604A-slides/"
-sldat <- data.frame('W' = week, 
-                    Slides = paste0("[",names,"](",url, slides,")"),
+sldat <- data.frame('S' = index0, 
+                    Content = names,
+                    Slides = sl,
+                    PDF = slpdf,
                     Videos =  paste0("[videos](", video, ")"),
                     SAS = codestr,
                     R = codeRstr)
