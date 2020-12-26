e70f8ca4f46a0fafd246a668092db4c90e290653
Léo Belzile <belzilel@gmail.com>
2020-12-25 21:06:17 -0500

    Fix code random slope

--- code/60604A_random_slope.R
+++ code/60604A_random_slope.R
@@ -1,4 +1,8 @@
 library(lme4)
+library(hecstatmod)
+
+
+## Example 1: 
 data(chicken, package = "hecstatmod")
 # Fit a model with diet as fixed effect
 # Correlated random effects
@@ -13,30 +17,26 @@ mod2 <- lme4::lmer(weight ~  time + diet +
 # H0: omega_{12}=0 versus H1: omega_{12} NEQ 0
 anova(mod1, mod2)
 
+
 # Fit alternative with a random effect for diet 
 # chick is nested within diet
+# With categorical variables, you must remove the fixed
+# effect in order to estimate the random effect
 mod3 <- lme4::lmer(weight ~ time +
                      (1 + time | diet:chick) + 
                      (1 | diet),
                    data = chicken)
 
-# Function from StackOverflow
-# https://stackoverflow.com/questions/45650548/get-residual-variance-covariance-matrix-in-lme4/45655597#45655597
-rescov <- function(model, data) {
-  var.d <- crossprod(getME(model,"Lambdat"))
-  Zt <- getME(model,"Zt")
-  vr <- sigma(model)^2
-  var.b <- vr*(t(Zt) %*% var.d %*% Zt)
-  sI <- vr * Diagonal(nrow(data))
-  var.y <- var.b + sI
-  invisible(var.y)
-}
+res3 <- rescov(mod3)
 # Print the covariance matrix of the nested models
-rc3 <- rescov(mod3, chicken)
+plot(res3, corr = FALSE)
 # Plot the correlation matrix
-image(cov2cor(rc3), sub = "", xlab = "", ylab = "")
+plot(res3, corr = TRUE)
+# Plot sub-matrix for a given chicken
+p38 <- chicken$chick == 38
+image(cov2cor(res3$var_y[p38,p38]))
 
-### Example with motivation data
+### Example 2 with motivation data
 # Why shouldn't we put a random effect for sex?
 data(motivation, package = "hecstatmod")
 mod1 <- lme4::lmer(motiv ~ yrserv + agemanager + nunit + (1 | sex) + (1 | idunit),
